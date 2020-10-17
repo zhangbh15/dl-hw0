2efe9964d75621480ee924e80b3030944b0d516e
root <root@DESKTOP-QFQEMPI.localdomain>
2020-10-16 23:22:37 -0700

    2322

--- src/connected_layer.c
+++ src/connected_layer.c
@@ -38,7 +38,10 @@ void backward_bias(matrix delta, matrix db)
 matrix forward_connected_layer(layer l, matrix in)
 {
     // TODO: 3.1 - run the network forward
-    matrix out = make_matrix(in.rows, l.w.cols); // Going to want to change this!
+    // matrix out = make_matrix(in.rows, l.w.cols); // Going to want to change this!
+    matrix out = matmul(in, l.w);
+    forward_bias(out, l.b);
+    activate_matrix(out, l.activation);
 
     // Saving our input and output and making a new delta matrix to hold errors
     // Probably don't change this
@@ -62,17 +65,24 @@ void backward_connected_layer(layer l, matrix prev_delta)
     // TODO: 3.2
     // delta is the error made by this layer, dL/dout
     // First modify in place to be dL/d(in*w+b) using the gradient of activation
-    
+    matrix dxwpb = copy_matrix(delta);
+    gradient_matrix(out, l.activation, dxwpb);
+
     // Calculate the updates for the bias terms using backward_bias
     // The current bias deltas are stored in l.db
+    backward_bias(dxwpb, l.db);
 
     // Then calculate dL/dw. Use axpy to add this dL/dw into any previously stored
     // updates for our weights, which are stored in l.dw
+    matrix dw = matmul(transpose_matrix(in),dxwpb);
+    axpy_matrix(-1.0, dw, l.dw);
 
     if(prev_delta.data){
         // Finally, if there is a previous layer to calculate for,
         // calculate dL/d(in). Again, using axpy, add this into the current
         // value we have for the previous layers delta, prev_delta.
+        matrix dx = matmul(dxwpb, transpose_matrix(l.w));
+        axpy_matrix(1.0, dx, prev_delta);
     }
 }
 
@@ -80,6 +90,13 @@ void backward_connected_layer(layer l, matrix prev_delta)
 void update_connected_layer(layer l, float rate, float momentum, float decay)
 {
     // TODO
+    axpy_matrix(-decay, l.w, l.dw);
+ 
+    axpy_matrix(rate, l.dw, l.w);
+    axpy_matrix(rate, l.db, l.b);
+    
+    scal_matrix(momentum, l.dw);
+    scal_matrix(momentum, l.db);
 }
 
 layer make_connected_layer(int inputs, int outputs, ACTIVATION activation)
